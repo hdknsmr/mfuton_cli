4dd14714376f7f6f36d9a47c448adbe988142e81
NISHIMURA Hideki <hideki@westvill.net>
2016-08-23 10:46:35 +0900

    add cloudwatcher.js

---
+++ RasPI-nodejs/cloudwatcher.js
@@ -0,0 +1,36 @@
+var firebase = require("firebase");
+var db;
+var ref;
+
+function _init() {
+	// Initialize the app with no authentication
+	firebase.initializeApp({
+		databaseURL: "https://magicfuton.firebaseio.com/"
+	});
+
+	// The app only has access to public data as defined in the Security Rules
+	db = firebase.database();
+	ref = db.ref("/status");
+}
+
+function _on(ev, cb) {
+	// console.log(ref);
+	ref.once(ev, cb);
+}
+
+module = function() {
+	_init();
+	return {
+		_ref : ref,
+		onValue : function(cb) {
+			_on("value", cb);
+		}
+	};
+}
+
+if (process.argv[1].match(/cloudwatcher/)) {
+	m = module();
+	m.onValue(function(snapshot) {
+		console.log(snapshot.val());
+	});
+}
